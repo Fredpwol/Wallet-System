7dd361b10e5629283f0de99b87a6b44206d93286
Fredpwol <cyborg290@gmail.com>
2021-04-30 23:21:19 +0100

    Imported models

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -9,3 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_object(os.environ["APP_CONFIG"])
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from api.model import *
