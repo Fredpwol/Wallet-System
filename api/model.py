defe8d0ec84b6ab460809609c9b1bb960b0d0aa7
Fredpwol <cyborg290@gmail.com>
2021-05-01 22:04:20 +0100

    fixed some bugs

--- api/model.py
+++ api/model.py
@@ -1,5 +1,6 @@
 
 from api import db, bcrypt, app
+from api.utils import CurrencyUtils
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
 
 
@@ -82,20 +83,22 @@ class Wallet(db.Model):
             "currency": self.currency,
             "owner": self.owner.serialize,
             "balance": self.balance,
-            "transa"
+            "transactions": [tx.serialize for tx in self.sent_transactions.union(self.received_transaction).order_by(Transaction.at.desc())]
         }
 
     @property
     def balance(self):
-        sent = self.sent_transactions.filter_by(isapproved=True)
-        received = self.received_transaction.filter_by(
-            receiver=self.owner.id).filter_by(isapproved=True)
+        sent = [CurrencyUtils.convert_currency(
+            tx.currency, self.currency, tx.amount) for tx in self.sent_transactions.filter_by(isapproved=True)]
+        received = [CurrencyUtils.convert_currency(
+            tx.currency, self.currency, tx.amount) for tx in self.received_transaction.filter_by(isapproved=True)]
         total_sent = 0.0
         total_received = 0.0
+        print(sent, received)
         for tx in sent:
-            total_sent += tx.amount
+            total_sent += tx
         for tx in received:
-            total_received += tx.amount
+            total_received += tx
         return total_received - total_sent
 
     def __repr__(self):
@@ -107,13 +110,28 @@ class Transaction(db.Model):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
+    at = db.Column(db.DateTime())
     sender = db.Column(db.Integer, db.ForeignKey(
-        "wallets.id",  ondelete="SET NULL"), nullable=False)
+        "wallets.id",  ondelete="SET NULL"))
     receiver = db.Column(db.Integer, db.ForeignKey(
-        "wallets.id",  ondelete="SET NULL"), nullable=False)
+        "wallets.id",  ondelete="SET NULL"))
     amount = db.Column(db.Float, nullable=False)
     currency = db.Column(db.String(16), nullable=False)
     isapproved = db.Column(db.Boolean, default=True)
+
+    @property
+    def serialize(self):
+        sender = None
+        if self.sender is not None:
+            sender = Wallet.query.get(self.sender).owner.serialize
+        return {
+            "id": self.id,
+            "sender": sender,
+            "receiver": Wallet.query.get(self.receiver).owner.serialize,
+            "amount": self.amount,
+            "currency": self.currency,
+            "isapproved": self.isapproved
+        }
 
     def __repr__(self):
         return f"Transaction({self.sender}, {self.receiver}, {self.amount})"
@@ -128,6 +146,24 @@ class Permissions:
     CHANGE_ROLE = 0X20
 
 
+roles = {
+    "Noob": [
+        (Permissions.OWN_WALLET | Permissions.CAN_WITHDRAW),
+        True
+    ],
+    "Elite": [
+        (Permissions.OWN_WALLET | Permissions.CAN_WITHDRAW |
+            Permissions.CREATE_WALLET),
+        False
+    ],
+    "Admin": [
+        (Permissions.APRROVE_FUNDS |
+            Permissions.CHANGE_CURRENCY | Permissions.CHANGE_ROLE),
+        False
+    ]
+}
+
+
 class Role(db.Model):
 
     __tablename__ = "roles"
@@ -140,23 +176,6 @@ class Role(db.Model):
 
     @staticmethod
     def initialize_roles():
-        roles = {
-            "Noob": [
-                (Permissions.OWN_WALLET | Permissions.CAN_WITHDRAW),
-                True
-            ],
-            "Elite": [
-                (Permissions.OWN_WALLET | Permissions.CAN_WITHDRAW |
-                 Permissions.CREATE_WALLET),
-                False
-            ],
-            "Admin": [
-                (Permissions.APRROVE_FUNDS |
-                 Permissions.CHANGE_CURRENCY | Permissions.CHANGE_ROLE),
-                False
-            ]
-        }
-
         for role in roles:
             if Role.query.filter_by(name=role).first() is None:
                 r = Role(
