6870ab26b6a23bffa895e7d733dfd535acb1cbf1
Fredpwol <cyborg290@gmail.com>
2021-05-01 02:59:14 +0100

    used classes

--- api/utils.py
+++ api/utils.py
@@ -2,11 +2,14 @@ import os
 import requests
 
 
-def iscurrency_valid(currency):
-    url = f"http://data.fixer.io/api/latest?access_key={os.environ['FIXER_API_KEY']}"
-    try :
-        res = requests.get(url)
-        rates = res.json()["rates"]
-        return currency.upper() in rates.keys()
-    except Exception:
-        return False
+class CurrencyUtils:
+    
+    @staticmethod
+    def iscurrency_valid(currency):
+        url = f"http://data.fixer.io/api/latest?access_key={os.environ['FIXER_API_KEY']}"
+        try :
+            res = requests.get(url)
+            rates = res.json()["rates"]
+            return currency.upper() in rates.keys()
+        except Exception:
+            return False
