e275e7c4318c1b8589de257656921ed07c3d0ebd
Fredpwol <cyborg290@gmail.com>
2021-05-01 22:04:00 +0100

    changed api from fixer to exchagerate-api and added convert function

--- api/utils.py
+++ api/utils.py
@@ -3,13 +3,27 @@ import requests
 
 
 class CurrencyUtils:
-    
     @staticmethod
     def iscurrency_valid(currency):
-        url = f"http://data.fixer.io/api/latest?access_key={os.environ['FIXER_API_KEY']}"
-        try :
+        url = f"https://v6.exchangerate-api.com/v6/{os.environ['API_KEY']}/codes"
+        try:
             res = requests.get(url)
-            rates = res.json()["rates"]
-            return currency.upper() in rates.keys()
+            codes = res.json()["supported_codes"]
+            valid_code = False
+            for code in codes:
+                if code[0].lower() == currency.lower():
+                    valid_code = True
+                    break
+            return valid_code
+        except Exception:
+            return False
+
+    @staticmethod
+    def convert_currency(_from, _to, amount):
+        url = f"https://v6.exchangerate-api.com/v6/{os.environ['API_KEY']}/pair/{_from.upper()}/{_to.upper()}/{amount}"
+        try:
+            res = requests.get(url)
+            value = float(res.json()["conversion_result"])
+            return value
         except Exception:
             return False
