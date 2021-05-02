e4400e796ee99bfebc4bdb32ac1a52ab91d233b6
Fredpwol <cyborg290@gmail.com>
2021-05-02 15:14:38 +0100

    removed exception handlers

--- api/utils.py
+++ api/utils.py
@@ -6,24 +6,18 @@ class CurrencyUtils:
     @staticmethod
     def iscurrency_valid(currency):
         url = f"https://v6.exchangerate-api.com/v6/{os.environ['API_KEY']}/codes"
-        try:
-            res = requests.get(url)
-            codes = res.json()["supported_codes"]
-            valid_code = False
-            for code in codes:
-                if code[0].lower() == currency.lower():
-                    valid_code = True
-                    break
-            return valid_code
-        except Exception:
-            return False
+        res = requests.get(url)
+        codes = res.json()["supported_codes"]
+        valid_code = False
+        for code in codes:
+            if code[0].lower() == currency.lower():
+                valid_code = True
+                break
+        return valid_code
 
     @staticmethod
     def convert_currency(_from, _to, amount):
         url = f"https://v6.exchangerate-api.com/v6/{os.environ['API_KEY']}/pair/{_from.upper()}/{_to.upper()}/{amount}"
-        try:
-            res = requests.get(url)
-            value = float(res.json()["conversion_result"])
-            return value
-        except Exception:
-            return False
+        res = requests.get(url)
+        value = float(res.json()["conversion_result"])
+        return value
