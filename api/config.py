6c05b36a78af063bf0632e39efec1815ff9c4d36
Fredpwol <cyborg290@gmail.com>
2021-05-03 04:13:39 +0100

    replace old postgres uri scheme

--- api/config.py
+++ api/config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
 
 
 class ProductionConfig(Config):
